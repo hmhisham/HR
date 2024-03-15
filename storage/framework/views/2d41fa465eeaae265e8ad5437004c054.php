58af851e7672a984f0a5a61fbc49e01e0fc0e58c
Hisham Ahmed <hm.hisham@gmail.com>
2024-03-15 09:02:22 +0300

    employees

---
+++ storage/framework/views/2d41fa465eeaae265e8ad5437004c054.php
@@ -0,0 +1,88 @@
+<!-- Remove Role Modal -->
+<div wire:ignore.self class="modal fade" id="removeRoleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-xl modal-dialog-centered modal-add-new-role">
+		<div class="p-3 modal-content p-md-5">
+			<button type="button" class="btn-close btn-pinned" data-bs-dismiss="modal" aria-label="Close"></button>
+			<div class="modal-body p-md-0">
+				<div class="mb-4 text-center">
+					<h3 class="pb-0 mb-2 role-title text-danger">حذف الدور</h3>
+					<p>حذف الدور مع صلاحياته</p>
+				</div>
+				<!-- Remove role form -->
+				<form id="removeRoleForm" class="row g-3">
+					<div class="mb-1 col-12">
+						<div class="form-floating form-floating-outline">
+							<input wire:model.defer='name' type="text" id="RoleNameModal" name="RoleNameModal" autofocus
+								class="form-control <?php if(strlen($name) > 0): ?> is-filled <?php endif; ?> <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid is-filled <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"
+								placeholder="أسم الدور" tabindex="-1" disabled/>
+							<label for="RoleNameModal">أسم الدور</label>
+						</div>
+						<?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                            <small class='text-danger inputerror'> <?php echo e($message); ?> </small>
+                        <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+					</div>
+					<div class="mb-3 col-12">
+						<h5>صلاحيات الدور</h5>
+						<div class="d-flex justify-content-center">
+							<div class="mx-1 text-nowrap fw-semibold">صلاحيات المسؤول 
+								<i class="mdi mdi-information-outline" data-bs-toggle="tooltip"
+									data-bs-placement="top" title="يسمح بالوصول الكامل إلى النظام"></i>
+							</div>
+							<div class="mx-1"> 
+								<label class="switch switch-primary">
+									<input wire:click='CheckAll' type="checkbox" class="switch-input" id="selectAll" <?php echo e($CheckAll); ?> disabled/>
+									<span class="switch-toggle-slider">
+										<span class="switch-on"></span>
+										<span class="switch-off"></span>
+									</span>
+									<span class="switch-label">تحديد كل الأدوار</span>
+								</label>
+							</div>
+						</div>
+					</div>
+					<div class="col-12">
+						<div class="row">
+							<?php $__currentLoopData = $Permissions; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $Permission): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+								<div class="mb-3 col-3">
+									<label class="switch switch-primary">
+										<input wire:model.defer='SetPermission.<?php echo e($Permission->id); ?>' type="checkbox" value="<?php echo e($Permission->name); ?>" class="switch-input" disabled />
+										<span class="switch-toggle-slider">
+											<span class="switch-on"></span>
+											<span class="switch-off"></span>
+										</span>
+										<span class="switch-label text-dark fw-bolder"><?php echo e($Permission->name); ?></span>
+									</label>
+								</div>
+							<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+						</div>
+					</div>
+					<div class="text-center col-12">
+						<hr>
+						<button wire:click='destroy' type="button" class="btn btn-danger me-sm-3 me-1">حذف الدور</button>
+						<button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal"
+							aria-label="Close">تجاهل</button>
+					</div>
+				</form>
+				<!--/ Remove role form -->
+			</div>
+		</div>
+	</div>
+</div>
+<!--/ Remove Role Modal -->
+
+
+<?php /**PATH F:\LaravelProjects\HR\resources\views/livewire/owner/permissions-roles/roles/modals/remove-role.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/98de32eecf4742d3e8a1186e51731be3.php
@@ -0,0 +1,110 @@
+<?php $__env->startSection('title', 'ادوار المستخدمين'); ?>
+
+<?php $__env->startSection('vendor-style'); ?>
+	<link rel="stylesheet" href="<?php echo e(asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css')); ?>">
+	<link rel="stylesheet" href="<?php echo e(asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css')); ?>">
+	<link rel="stylesheet" href="<?php echo e(asset('assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css')); ?>">
+	<link rel="stylesheet" href="<?php echo e(asset('assets/vendor/libs/formvalidation/dist/css/formValidation.min.css')); ?>" />
+
+	<link rel="stylesheet" href="<?php echo e(asset('assets/vendor/libs/animate-css/animate.css')); ?>" />
+	<link rel="stylesheet" href="<?php echo e(asset('assets/vendor/libs/sweetalert2/sweetalert2.css')); ?>" />
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+
+	<?php
+if (! isset($_instance)) {
+    $html = \Livewire\Livewire::mount('owner.permissions-roles.roles.account-roles')->html();
+} elseif ($_instance->childHasBeenRendered('tYvFVnb')) {
+    $componentId = $_instance->getRenderedChildComponentId('tYvFVnb');
+    $componentTag = $_instance->getRenderedChildComponentTagName('tYvFVnb');
+    $html = \Livewire\Livewire::dummyMount($componentId, $componentTag);
+    $_instance->preserveRenderedChild('tYvFVnb');
+} else {
+    $response = \Livewire\Livewire::mount('owner.permissions-roles.roles.account-roles');
+    $html = $response->html();
+    $_instance->logRenderedChild('tYvFVnb', $response->id(), \Livewire\Livewire::getRootElementTagName($html));
+}
+echo $html;
+?>
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('vendor-script'); ?>
+	<script src="<?php echo e(asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js')); ?>"></script>
+
+	<script src="<?php echo e(asset('assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js')); ?>"></script>
+	<script src="<?php echo e(asset('assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js')); ?>"></script>
+	<script src="<?php echo e(asset('assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js')); ?>"></script>
+
+	<script src="<?php echo e(asset('assets/vendor/libs/sweetalert2/sweetalert2.js')); ?>"></script>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('page-script'); ?>
+	<script src="<?php echo e(asset('assets/js/app-access-roles.js')); ?>"></script>
+	<script src="<?php echo e(asset('assets/js/modal-add-role.js')); ?>"></script>
+
+	<script src="<?php echo e(asset('assets/js/extended-ui-sweetalert2.js')); ?>"></script>
+	<script src="<?php echo e(asset('assets/js/form-basic-inputs.js')); ?>"></script>
+
+	<script>
+		const Toast = Swal.mixin({
+			toast: true,
+			position: 'top-start',
+			showConfirmButton: false,
+			timer: 3000,
+			timerProgressBar: true,
+			didOpen: (toast) => {
+				toast.addEventListener('mouseenter', Swal.stopTimer)
+				toast.addEventListener('mouseleave', Swal.resumeTimer)
+			}
+		})
+		window.addEventListener('RoleModalShow', event => 
+            {
+                $("#RoleNameModal").focus();
+            }
+        );
+
+		window.addEventListener('RoleAddSuccess', 
+            event => {
+				Toast.fire({
+					icon: 'success',
+					title: 'تم اضافة الدور بنجاح'
+				})
+
+				$("#RoleNameModal").focus();
+            }
+        );
+		window.addEventListener('RoleAddError', 
+            event => {
+				Toast.fire({
+					icon: 'error',
+					title: 'لم يتم تحديد صلاحيات للدور'
+				})
+
+				$("#RoleNameModal").focus();
+            }
+        );
+		window.addEventListener('RoleUpdateSuccess', 
+            event => {
+                $('#editRoleModal').modal('hide');
+
+				Toast.fire({
+					icon: 'success',
+					title: 'تم تعديل الدور بنجاح'
+				})
+            }
+        );
+		window.addEventListener('RoleDestroySuccess', 
+            event => {
+                $('#removeRoleModal').modal('hide');
+
+				Toast.fire({
+					icon: 'success',
+					title: 'تم حذف الدور بنجاح'
+				})
+            }
+        );
+	</script>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts/layoutMaster', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\LaravelProjects\HR\resources\views/content/Owner/Permission&Roles/Roles/index.blade.php ENDPATH**/ ?>
