73f00751a39b707b121f770ccfe915c618a7fc1e
Hisham Ahmed <hm.hisham@gmail.com>
2024-09-20 22:20:22 +0300

    Hisham 20-09-24 10:20

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
+use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EMP\EMPController;
+
 use App\Http\Controllers\Areas\AreasController;
+
+
 use App\Http\Controllers\Units\UnitsController;
 
+use App\Http\Controllers\Wives\WivesController;
 use App\Http\Controllers\Branch\BranchController;
 
 use App\Http\Controllers\Grades\GradesController;
+
 use App\Http\Controllers\Thanks\ThanksController;
 use App\Http\Controllers\Coaches\CoachesController;
 use App\Http\Controllers\Courses\CoursesController;
@@ -56,8 +63,10 @@ use App\Http\Controllers\Specializationclassification\Specializationclassificati
 |
 */
 
+// Dashboard
+
+
 Route::middleware(['auth', config('jetstream.auth_session'), 'verified'])->group(function () {
-    // Dashboard
     Route::GET('/', [DashboardController::class, 'index'])->name('Dashboard');
 
     // Middleware Owners
@@ -72,7 +81,8 @@ Route::middleware(['auth', config('jetstream.auth_session'), 'verified'])->group
         Route::RESOURCE('Administrators-Accounts', AdministratorsAccountsController::class);
         Route::RESOURCE('Users-Accounts', UsersAccountsController::class);
         Route::RESOURCE('Customers-Accounts', CustomersAccountsController::class);
-    });
+
+
 
         // المحافظات I
         Route::RESOURCE('Governorates', GovernoratesController::class);
@@ -83,7 +93,7 @@ Route::middleware(['auth', config('jetstream.auth_session'), 'verified'])->group
         // مكتب المعلومات
         Route::RESOURCE('Infooffice', InfoofficeController::class);
         //الارتباط
-        Route::  RESOURCE('Linkages', LinkagesController::class);
+        Route::RESOURCE('Linkages', LinkagesController::class);
         //القسم
         Route::RESOURCE('Sections', SectionsController::class);
         //الشعبة
@@ -108,6 +118,8 @@ Route::middleware(['auth', config('jetstream.auth_session'), 'verified'])->group
         Route::RESOURCE('Scalems', ScalemsController::class);
         // العقود السلم الوظيفي
         Route::RESOURCE('Scaleas', ScaleasController::class);
+        // سلم عقود الفنيين
+        Route::RESOURCE('Technicians', TechniciansController::class);
         //مجال التدريب
         Route::RESOURCE('Trainings', TrainingsController::class);
         //نوع الاجازة
@@ -117,12 +129,13 @@ Route::middleware(['auth', config('jetstream.auth_session'), 'verified'])->group
         //نوع الخدمة
         Route::RESOURCE('Typesservices', TypesservicesController::class);
         // Route::  RESOURCE('Workers', WorkersController::class);
-
+        // الدوائر
+        Route::RESOURCE('Department', DepartmentController::class);
         // بيانات الموظف
         Route::GET('Workers', [WorkersController::class, 'index'])->name('Workers');
         Route::GET('AddWorker', [WorkersController::class, 'AddWorker'])->name('AddWorker');
 
-        // التشكرات
+        // التشكراتj
         Route::RESOURCE('Thanks', ThanksController::class);
         // عقوبات
         Route::RESOURCE('Penalties', PenaltiesController::class);
@@ -137,7 +150,9 @@ Route::middleware(['auth', config('jetstream.auth_session'), 'verified'])->group
         // المدربين
         Route::RESOURCE('Coaches', CoachesController::class);
         // الدورات
-        Route::  RESOURCE('Courses', CoursesController::class);
+        Route::RESOURCE('Courses', CoursesController::class);
+        // الزوجة
+        Route::RESOURCE('Wives', WivesController::class);
     });
 });
 
