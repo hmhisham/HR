1cd12bb6109f4f347da8986468846e5486ad4d95
8yas9 <49659862+8yas9@users.noreply.github.com>
2024-09-27 14:30:16 +0300

    test

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use \App\Http\Controllers\API\Login\LoginController;
 use \App\Http\Controllers\API\Attendance\AttendanceController;
 
 Route::POST('Login',[LoginController::class,'login']);
-// Route::POST('Attendance',[AttendanceController::class,'attendance']);
 Route::get('/attendance/{calculator_number}', [AttendanceController::class, 'getAttendance']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
