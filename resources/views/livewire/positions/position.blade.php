26594eb136d8147fc7d8cbdb9b4b340deb780228
muntedher2022 <muntedher099@gmail.com>
2024-10-23 23:31:40 +0300

    mun 23-10

--- resources/views/livewire/positions/position.blade.php
+++ resources/views/livewire/positions/position.blade.php
@@ -18,60 +18,58 @@
                 </div>
             </div>
         </div>
-        <div class="card-body">
-            @can('position-list')
-                <table class="table">
-                    <thead class="table-light">
+        @can('position-list')
+            <table class="table">
+                <thead class="table-light">
+                    <tr>
+                        <th>#</th>
+                        <th class="text-center">الاسم</th>
+                        <th class="text-center">اسم المنصب</th>
+                        <th class="text-center">رقم امر التكليف</th>
+                        <th class="text-center">العملية</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 0; ?>
+                    @foreach ($Positions as $Position)
                         <tr>
-                            <th>#</th>
-                            <th class="text-center">الاسم</th>
-                            <th class="text-center">اسم المنصب</th>
-                            <th class="text-center">رقم امر التكليف</th>
-                            <th class="text-center">العملية</th>
+                            <?php $i++; ?>
+                            <td>{{ $i }}</td>
+                            <td class="text-center">{{ $Position->Getworker ? $Position->Getworker->full_name : '' }}
+                            </td>
+                            <td class="text-center">{{ $Position->position_name }}</td>
+                            <td class="text-center">{{ $Position->position_order_number }}</td>
+                            <td class="text-center">
+                                <div class="btn-group" role="group" aria-label="First group">
+                                    @can('position-edit')
+                                        <button wire:click="GetPosition({{ $Position->id }})"
+                                            class="p-0 px-1 btn btn-outline-success waves-effect" data-bs-toggle="modal"
+                                            data-bs-target="#editpositionModal">
+                                            <i class="tf-icons mdi mdi-pencil fs-3"></i>
+                                        </button>
+                                    @endcan
+                                    @can('position-delete')
+                                        <button wire:click="GetPosition({{ $Position->id }})"
+                                            class="p-0 px-1 btn btn-outline-danger waves-effect {{ $Position->active ? 'disabled' : '' }}"
+                                            data-bs-toggle = "modal" data-bs-target="#removepositionModal">
+                                            <i class="tf-icons mdi mdi-delete-outline fs-3"></i>
+                                        </button>
+                                    @endcan
+                                </div>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <?php $i = 0; ?>
-                        @foreach ($Positions as $Position)
-                            <tr>
-                                <?php $i++; ?>
-                                <td>{{ $i }}</td>
-                                <td class="text-center">{{ $Position->Getworker ? $Position->Getworker->full_name : '' }}
-                                </td>
-                                <td class="text-center">{{ $Position->position_name }}</td>
-                                <td class="text-center">{{ $Position->position_order_number }}</td>
-                                <td class="text-center">
-                                    <div class="btn-group" role="group" aria-label="First group">
-                                        @can('position-edit')
-                                            <button wire:click="GetPosition({{ $Position->id }})"
-                                                class="p-0 px-1 btn btn-outline-success waves-effect" data-bs-toggle="modal"
-                                                data-bs-target="#editpositionModal">
-                                                <i class="tf-icons mdi mdi-pencil fs-3"></i>
-                                            </button>
-                                        @endcan
-                                        @can('position-delete')
-                                            <button wire:click="GetPosition({{ $Position->id }})"
-                                                class="p-0 px-1 btn btn-outline-danger waves-effect {{ $Position->active ? 'disabled' : '' }}"
-                                                data-bs-toggle = "modal" data-bs-target="#removepositionModal">
-                                                <i class="tf-icons mdi mdi-delete-outline fs-3"></i>
-                                            </button>
-                                        @endcan
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                    @endforeach
+                </tbody>
+            </table>
 
-                <div class="mt-2 d-flex justify-content-center">
-                    {{ $links->links() }}
-                </div>
+            <div class="mt-2 d-flex justify-content-center">
+                {{ $links->links() }}
+            </div>
 
-                <!-- Modal -->
-                @include('livewire.positions.modals.edit-position')
-                @include('livewire.positions.modals.remove-position')
-                <!-- Modal -->
-            @endcan
-        </div>
+            <!-- Modal -->
+            @include('livewire.positions.modals.edit-position')
+            @include('livewire.positions.modals.remove-position')
+            <!-- Modal -->
+        @endcan
     </div>
 </div>
