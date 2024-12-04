fead0db0139191c6f72040f513d873f84f39ba08
muntedher2022 <muntedher099@gmail.com>
2024-12-04 21:02:08 +0300

    mun boycott

--- resources/views/livewire/boycotts/boycott.blade.php
+++ resources/views/livewire/boycotts/boycott.blade.php
@@ -42,14 +42,14 @@
                                 <div class="btn-group" role="group" aria-label="First group">
                                     @can('boycott-edit')
                                         <button wire:click="GetBoycott({{ $Boycott->id }})"
-                                            class="p-0 px-1 btn btn-outline-success waves-effect" data-bs-toggle="modal"
+                                            class="p-0 px-1 btn btn-text-success waves-effect" data-bs-toggle="modal"
                                             data-bs-target="#editboycottModal">
                                             <i class="tf-icons mdi mdi-pencil fs-3"></i>
                                         </button>
                                     @endcan
                                     @can('boycott-delete')
                                         <button wire:click="GetBoycott({{ $Boycott->id }})"
-                                            class="p-0 px-1 btn btn-outline-danger waves-effect {{ $Boycott->active ? 'disabled' : '' }}"
+                                            class="p-0 px-1 btn btn-text-danger waves-effect {{ $Boycott->active ? 'disabled' : '' }}"
                                             data-bs-toggle = "modal" data-bs-target="#removeboycottModal">
                                             <i class="tf-icons mdi mdi-delete-outline fs-3"></i>
                                         </button>
