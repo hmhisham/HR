cf09418e168a637606101baa2bfc8dfa8e4d7c03
Hisham Ahmed <hm.hisham@gmail.com>
2024-12-28 17:26:46 +0300

    HISHAM 05:26

--- resources/views/layouts/sections/scriptsIncludes.blade.php
+++ resources/views/layouts/sections/scriptsIncludes.blade.php
@@ -9,6 +9,7 @@
 
   <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
   <script src="{{ asset('assets/js/config.js') }}"></script>
+  <script src="{{ asset('assets/vendor/js/preloader.js') }}"></script>
 
 @if ($configData['hasCustomizer'])
   <script>
