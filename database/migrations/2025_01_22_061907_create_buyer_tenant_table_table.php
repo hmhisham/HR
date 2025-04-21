7df30c62402441a3dd55e0ec0d44e25de791dc63
8yas9 <yasksalim@gmail.com>
2025-04-21 13:07:03 +0300

    mun 321

--- database/migrations/2025_01_22_061907_create_buyer_tenant_table_table.php
+++ database/migrations/2025_01_22_061907_create_buyer_tenant_table_table.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-<<<<<<< HEAD
     /**
      * Run the migrations.
      */
@@ -36,26 +35,6 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-=======
-  /**
-   * Run the migrations.
-   */
-  public function up(): void
-  {
-    Schema::create('buyer_tenant', function (Blueprint $table) {
-      $table->id();
-      $table->integer('user_id')->comment('رقم المستخدم');
-      $table->string('property_number')->comment('رقم العقار');
-      $table->string('buyer_tenant_name')->comment('أسم المشتري أو المستأجر');
-      $table->string('buyer_computer_number')->nullable()->comment('رقم الحاسبة للمشتري');
-      $table->string('buyer_tenant_phone_number')->comment('رقم هاتف المشتري أو المستأجر');
-      $table->string('buyer_tenant_email')->nullable()->comment('البريد الالكتروني للمشتري أو المستأجر');
-      $table->string('notes')->nullable()->comment('الملاحظات');
-      $table->string('buyer_tenant_type')->nullable()->comment('مشتري أم مستأجر');
-      $table->timestamps();
-    });
-  }
->>>>>>> 00f11ddd74f76cbca93548f9f3b4de63da9f751c
 
   /**
    * Reverse the migrations.
