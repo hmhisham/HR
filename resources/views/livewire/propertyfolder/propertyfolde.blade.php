f600d0a84045a94361f71ff74ab7b7cceef21bfc
8yas9 <yasksalim@gmail.com>
2025-09-19 23:07:40 +0300

    1

--- resources/views/livewire/propertyfolder/propertyfolde.blade.php
+++ resources/views/livewire/propertyfolder/propertyfolde.blade.php
@@ -1,139 +1,124 @@
-
-
 <div class="mt-n4">
-    <h4 class="mb-1 fw-semiboyld">قائمة \ اضبارة العقار</h4>
-    <div class="card">
+  <h4 class="mb-1 fw-semiboyld">قائمة \ اضبارة العقار</h4>
+  <div class="card">
 
     <div class="card">
-        <div class="card-header">
-            <div class="d-flex justify-content-between">
-                <div>
-                    <input wire:model="PropertyfoldeSearch" type="text" class="form-control" placeholder="بحث...">
-                </div>
-                <div>
-                    @can('propertyfolde-create')
-                        <button wire:click='AddPropertyfoldeModalShow' class="mb-3 add-new btn btn-primary mb-md-0"
-                            data-bs-toggle="modal" data-bs-target="#addpropertyfoldeModal">أضــافــة</button>
-                        @include('livewire.propertyfolder.modals.add-propertyfolde')
-                    @endcan
-                </div>
-            </div>
+      <div class="card-header">
+        <div class="d-flex justify-content-between">
+          <div>
+            <input wire:model="PropertyfoldeSearch" type="text" class="form-control" placeholder="بحث...">
+          </div>
+          <div>
+            @can('propertyfolde-create')
+            <button wire:click='AddPropertyfoldeModalShow' class="mb-3 add-new btn btn-primary mb-md-0" data-bs-toggle="modal" data-bs-target="#addpropertyfoldeModal">أضــافــة</button>
+            @include('livewire.propertyfolder.modals.add-propertyfolde')
+            @endcan
+          </div>
         </div>
-        @can('propertyfolde-list')
-            <table class="table">
-                <thead class="table-light">
-                    <tr>
-                     <th>#</th>
-                     <th class="text-center">رقم الاضبارة</th>
-<th class="text-center">اسم العقار</th>
-<th class="text-center">موقع العقار</th>
-<th class="text-center">نوع العقار</th>
-<th class="text-center">صفة العقار</th>
-<th class="text-center">مساحة العقار</th>
-<th class="text-center">رقم القطعة</th>
-<th class="text-center">اسم المقاطعة</th>
-<th class="text-center">الملاحظات</th>
-<th class="text-center">العملية</th> </tr>
+      </div>
+      @can('propertyfolde-list')
+      <table class="table">
+        <thead class="table-light">
+          <tr>
+            <th>#</th>
+            <th class="text-center">رقم الاضبارة</th>
+            <th class="text-center">اسم العقار</th>
+            <th class="text-center">موقع العقار</th>
+            <th class="text-center">نوع العقار</th>
+            <th class="text-center">صفة العقار</th>
+            <th class="text-center">مساحة العقار</th>
+            <th class="text-center">رقم القطعة</th>
+            <th class="text-center">اسم المقاطعة</th>
+            <th class="text-center">الملاحظات</th>
+            <th class="text-center">العملية</th>
+          </tr>
 
-                         <tr>
-                            <th></th>
-   <th class="text-center">
-                                <input type="text" wire:model.debounce.300ms="search.folder_number"
-                                    class="text-center form-control" placeholder="رقم الاضبارة">
-                            </th>
+          <tr>
+            <th></th>
+            <th class="text-center">
+              <input type="text" wire:model.debounce.300ms="search.folder_number" class="text-center form-control" placeholder="رقم الاضبارة">
+            </th>
 
-  <th class="text-center">
-                                <input type="text" wire:model.debounce.300ms="search.property_name"
-                                    class="text-center form-control" placeholder="اسم العقار">
-                            </th>
+            <th class="text-center">
+              <input type="text" wire:model.debounce.300ms="search.property_name" class="text-center form-control" placeholder="اسم العقار">
+            </th>
 
-  <th class="text-center">
-                                <input type="text" wire:model.debounce.300ms="search.id_property_location"
-                                    class="text-center form-control" placeholder="موقع العقار">
-                            </th>
+            <th class="text-center">
+              <input type="text" wire:model.debounce.300ms="search.id_property_location" class="text-center form-control" placeholder="موقع العقار">
+            </th>
 
-  <th class="text-center">
-                                <input type="text" wire:model.debounce.300ms="search.id_property_type"
-                                    class="text-center form-control" placeholder="نوع العقار">
-                            </th>
+            <th class="text-center">
+              <input type="text" wire:model.debounce.300ms="search.id_property_type" class="text-center form-control" placeholder="نوع العقار">
+            </th>
 
-  <th class="text-center">
-                                <input type="text" wire:model.debounce.300ms="search.id_property_description"
-                                    class="text-center form-control" placeholder="صفة العقار">
-                            </th>
+            <th class="text-center">
+              <input type="text" wire:model.debounce.300ms="search.id_property_description" class="text-center form-control" placeholder="صفة العقار">
+            </th>
 
-  <th class="text-center">
-                                <input type="text" wire:model.debounce.300ms="search.property_area"
-                                    class="text-center form-control" placeholder="مساحة العقار">
-                            </th>
+            <th class="text-center">
+              <input type="text" wire:model.debounce.300ms="search.property_area" class="text-center form-control" placeholder="مساحة العقار">
+            </th>
 
-  <th class="text-center">
-                                <input type="text" wire:model.debounce.300ms="search.plot_number"
-                                    class="text-center form-control" placeholder="رقم القطعة">
-                            </th>
+            <th class="text-center">
+              <input type="text" wire:model.debounce.300ms="search.plot_number" class="text-center form-control" placeholder="رقم القطعة">
+            </th>
 
-  <th class="text-center">
-                                <input type="text" wire:model.debounce.300ms="search.district_name"
-                                    class="text-center form-control" placeholder="اسم المقاطعة">
-                            </th>
+            <th class="text-center">
+              <input type="text" wire:model.debounce.300ms="search.district_name" class="text-center form-control" placeholder="اسم المقاطعة">
+            </th>
 
-  <th class="text-center">
-                                <input type="text" wire:model.debounce.300ms="search.notes"
-                                    class="text-center form-control" placeholder="الملاحظات">
-                            </th>
+            <th class="text-center">
+              <input type="text" wire:model.debounce.300ms="search.notes" class="text-center form-control" placeholder="الملاحظات">
+            </th>
 
-<th></th>
-                        </tr>
+            <th></th>
+          </tr>
 
 
-            </thead>
-            <tbody>
-                 <?php $i = 0; ?>
-                  @foreach ($Propertyfolder as $Propertyfolde)
-                           <tr>
-                                    <?php $i++; ?>
-                                    <td>{{ $i }}</td>
-                                   <td class="text-center">{{ $Propertyfolde->folder_number}}</td>
- <td class="text-center">{{ $Propertyfolde->property_name}}</td>
- <td class="text-center">{{ $Propertyfolde->id_property_location}}</td>
- <td class="text-center">{{ $Propertyfolde->id_property_type}}</td>
- <td class="text-center">{{ $Propertyfolde->id_property_description}}</td>
- <td class="text-center">{{ $Propertyfolde->property_area}}</td>
- <td class="text-center">{{ $Propertyfolde->plot_number}}</td>
- <td class="text-center">{{ $Propertyfolde->district_name}}</td>
- <td class="text-center">{{ $Propertyfolde->notes}}</td>
+        </thead>
+        <tbody>
+          <?php $i = 0; ?>
+          @foreach ($Propertyfolder as $Propertyfolde)
+          <tr>
+            <?php $i++; ?>
+            <td>{{ $i }}</td>
+            <td class="text-center">{{ $Propertyfolde->folder_number}}</td>
+            <td class="text-center">{{ $Propertyfolde->property_name}}</td>
+  <td class="text-center">{{ $Propertyfolde->Getprovince ? $Propertyfolde->Getprovince->province_name : '' }}</td>
+            <td class="text-center">{{ $Propertyfolde->id_property_type}}</td>
+            <td class="text-center">{{$Propertyfolde->id_property_description}}</td>
+            <td class="text-center">{{ $Propertyfolde->property_area}}</td>
+            <td class="text-center">{{ $Propertyfolde->plot_number}}</td>
+            <td class="text-center">{{ $Propertyfolde->district_name}}</td>
+            <td class="text-center">{{ $Propertyfolde->notes}}</td>
 
-                                  <td class="text-center">
-                                     <div class="btn-group" role="group" aria-label="First group">
-                                         @can('propertyfolde-edit')
-                                             <button wire:click="GetPropertyfolde({{ $Propertyfolde->id }})"
-                                                 class="p-0 px-1 btn btn-outline-success waves-effect" data-bs-toggle="modal"
-                                                 data-bs-target="#editpropertyfoldeModal">
-                                                 <i class="tf-icons mdi mdi-pencil fs-3"></i>
-                                             </button>
-                                         @endcan
-                                         @can('propertyfolde-delete')
-                                             <button wire:click="GetPropertyfolde({{ $Propertyfolde->id }})"
-                                                 class="p-0 px-1 btn btn-outline-danger waves-effect {{ $Propertyfolde->active ? 'disabled' : '' }}"
-                                                 data-bs-toggle = "modal" data-bs-target="#removepropertyfoldeModal">
-                                                 <i class="tf-icons mdi mdi-delete-outline fs-3"></i>
-                                             </button>
-                                         @endcan
-                                     </div>
-                                 </td>
-                  </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <div class="mt-2 d-flex justify-content-center">
-                {{ $links->links() }}
-            </div>
-            <!-- Modal -->
-            @include('livewire.propertyfolder.modals.edit-propertyfolde')
-            @include('livewire.propertyfolder.modals.remove-propertyfolde')
-            <!-- Modal -->
-        @endcan
+            <td class="text-center">
+              <div class="btn-group" role="group" aria-label="First group">
+                @can('propertyfolde-edit')
+                <button wire:click="GetPropertyfolde({{ $Propertyfolde->id }})" class="p-0 px-1 btn btn-outline-success waves-effect" data-bs-toggle="modal" data-bs-target="#editpropertyfoldeModal">
+                  <i class="tf-icons mdi mdi-pencil fs-3"></i>
+                </button>
+                @endcan
+                @can('propertyfolde-delete')
+                <button wire:click="GetPropertyfolde({{ $Propertyfolde->id }})" class="p-0 px-1 btn btn-outline-danger waves-effect {{ $Propertyfolde->active ? 'disabled' : '' }}" data-bs-toggle="modal" data-bs-target="#removepropertyfoldeModal">
+                  <i class="tf-icons mdi mdi-delete-outline fs-3"></i>
+                </button>
+                @endcan
+              </div>
+            </td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+      <div class="mt-2 d-flex justify-content-center">
+        {{ $links->links() }}
+      </div>
+      <!-- Modal -->
+      @include('livewire.propertyfolder.modals.edit-propertyfolde')
+      @include('livewire.propertyfolder.modals.remove-propertyfolde')
+      <!-- Modal -->
+      @endcan
     </div>
+  </div>
 </div>
 </div>
-   </div>
